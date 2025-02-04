48c226cca783003f428090d452adbd8c3d9afbc7
HugoS63 <naarutsu@outlook.fr>
2025-02-04 11:03:03 +0100

    code new function

--- module_01/checkV/bin/qc_visu.py
+++ module_01/checkV/bin/qc_visu.py
@@ -6,9 +6,10 @@ import matplotlib.pyplot as plt
 import os
 import pandas as pd
 import seaborn
+import subprocess
 
 
-def visual(file_quality, eco, tool, out)
+def visual(file_quality, eco, tool, out):
     """
     Function to create a barplot of the different quality categories from CheckV analysis.
 
@@ -19,7 +20,7 @@ def visual(file_quality, eco, tool, out)
     :return : None
     """
 
-    df_quality = pd.read_csv(file_quality, sep="\t", header=None)"
+    df_quality = pd.read_csv(file_quality, sep="\t", header=None)
 
     Lorder=["Complete","High-quality","Medium-quality","Low-quality","Not-determined"]
     Lcolor=["#07ce04","#8ffa37","#fffb00","#ff8513","#d4d4d4"]
@@ -29,12 +30,31 @@ def visual(file_quality, eco, tool, out)
     seaborn.countplot(x=1,data=df_quality,order=Lorder,palette=Lcolor)
     plt.savefig(f"{out}/{eco}_{tool}.png",format='png')
 
+    return None
 
 
-def parse_data(path, pathout)
+def parse_data(path, pathout):
+
     if not os.path.exists(pathout):
         os.makedirs(pathout)
 
+    tool_list = ["vs2", "dvf", "vibrant"]
+    for tool in tool_list:
+        res = subprocess.run(["ls", path+"/"+tool], capture_output=True, text=True)
+        re=res.stdout.split()
+
+        for folder in re:
+            eco=folder.split('_')[0]
+            file = subprocess.run(["ls",path+"/"+tool+"/"+folder+"/quality_summary.tsv"], capture_output=True, text=True)
+
+            try:
+                path_file=file.stdout.split()[0]
+            except:
+                print(f"Missing file 'quality_summary' in {path}/{tool}/{folder}")
+
+            visual(file, eco, tool, pathout)
+
+    return None
 
 
 if __name__ == '__main__':
@@ -51,3 +71,11 @@ if __name__ == '__main__':
     out = args.out
     path="module_01/checkV/results"
     out="module_01/checkV/figure"
+
+    print("Start the visualizations for quality...")
+
+    parse_data(path, out)
+
+    print("Job finish : figure created !")
+
+
