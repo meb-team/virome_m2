9735eb36cf9a17e0e37b9b50e4a217c94c1049a1
HugoS63 <naarutsu@outlook.fr>
2025-02-14 08:59:00 +0100

    fix

--- module_02/MMseq2/bin/taxo_annot.sh
+++ module_02/MMseq2/bin/taxo_annot.sh
@@ -17,8 +17,12 @@ set -o pipefail
 query="module_02/MMseq2/results/tax_fasta_seed.fa"
 DB="module_02/MMseq2/taxonomy/mmseqs_vrefseq/refseq_viral"
 OUTPUT="module_02/MMseq2/results"
+
 # === Process ===
-mmseqs createdb "$query" "$OUTPUT/indexed" # Creation of a mmseq2 format file from a fasta file
-mmseqs taxonomy "$OUTPUT/indexed" "$DB" "$OUTPUT" "$OUTPUT/tmp" --tax-lineage 1 --lca-mode 3 # annotation 
-mmseqs createtsv "$OUTPUT/indexed" "$OUTPUT" taxo_results.tsv # compute a TSV as result
-mmseqs taxonomyreport "$DB" "$OUTPUT" taxo_report
+mmseqs createdb "$query" "$OUTPUT/indexed"
+
+mmseqs taxonomy "$OUTPUT/indexed" "$DB" "$OUTPUT/res" "$OUTPUT/tmp" --tax-lineage 1 --lca-mode 3
+
+mmseqs createtsv $OUTPUT/indexed $OUTPUT taxo_results.tsv
+
+mmseqs taxonomyreport $DB $OUTPUT taxo_report
