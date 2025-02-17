6dce497d91c3f0a945b27b5b1aecc7c7bc481bd0
HugoS63 <naarutsu@outlook.fr>
2025-02-17 14:06:51 +0100

    opti

--- module_02/MMseq2/bin/building_res.py
+++ module_02/MMseq2/bin/building_res.py
@@ -1,52 +1,30 @@
 #!/usr/bin/env python3
 
 import argparse
-
-
-
 import pandas as pd
-
-# Chargement du fichier d'entrée
-input_taxo = "input_taxo.tsv"  # Fichier source
-output_taxo = "output_taxo.tsv"  # Fichier de sortie
-
-# Définition des colonnes taxonomiques
-columns = ["ID", "Domain", "Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"]
-
-# Initialisation d'une liste pour stocker les résultats
-data = []
-
-# Lecture et traitement du fichier
-with open(input_taxo, 'r') as f:
-    for line in f:
-        line = line.rstrip().split("\t")  # Séparation des colonnes du TSV
-        taxo_dict = {"ID": line[0], "Domain": "NA", "Kingdom": "NA", "Phylum": "NA", "Class": "NA", 
-                     "Order": "NA", "Family": "NA", "Genus": "NA", "Species": "NA"}  # Valeurs par défaut
-        
-        # Traitement de la classification taxonomique
-        for rank in line[-1].split(";"):
-            if rank[0] in ["d", "k", "p", "c", "o", "f", "g", "s"]:  # Vérifie si le préfixe est valide
-                key = {"d": "Domain", "k": "Kingdom", "p": "Phylum", "c": "Class", "o": "Order", 
-                       "f": "Family", "g": "Genus", "s": "Species"}[rank[0]]  # Conversion en colonne DataFrame
-                taxo_dict[key] = rank[2:]  # Extraction du nom taxonomique
-        
-        data.append(taxo_dict)  # Ajout de la ligne à la liste
-
-# Création du DataFrame
-df = pd.DataFrame(data, columns=columns)
-
-# Sauvegarde en TSV
-df.to_csv(output_taxo, sep="\t", index=False)
-
-print(f"✅ Fichier généré : {output_taxo}")
+import os
 
 
+def building_TSV(path):
+    columns = ["ID", "Domain", "Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"]
+    data = []
 
+    with open(path, 'r') as f:
+        for line in f:
+            line = line.rstrip().split("\t")
+            taxo_dict = {"ID": line[0], "Domain": "NA", "Kingdom": "NA", "Phylum": "NA", "Class": "NA",
+                     "Order": "NA", "Family": "NA", "Genus": "NA", "Species": "NA"}
 
+            for rank in line[-1].split(";"):
+                if rank[0] in ["d", "k", "p", "c", "o", "f", "g", "s"]:
+                    key = {"d": "Domain", "k": "Kingdom", "p": "Phylum", "c": "Class", "o": "Order",
+                       "f": "Family", "g": "Genus", "s": "Species"}[rank[0]]
+                    taxo_dict[key] = rank[2:]
+            data.append(taxo_dict)
 
+    df = pd.DataFrame(data, columns=columns)
 
-
-
+    return df
 
 
 if __name__ == '__main__':
@@ -56,19 +34,25 @@ if __name__ == '__main__':
 
     parser.add_argument("--path", "-p", help="Defines the path where the TSV taxo results is located. Usage : -p path folder/", type=str)
     parser.add_argument("--out", "-o", help="Defines the path where the rebuilding TSV created can be stored. Usage : -o path folder/", type=str)
-    parser.add_argument("--file", "-f", help"Defines the path where the TSV containing cluster informations is located. Usage : -f path/to/file", type=str)
 
     args = parser.parse_args()
 
     if args.path:
         path = args.path
     else:
-        path=""
-    if args.file:
-        file="module_01/filtering/results/filtered_representative_cluster.tsv"
+        path="module_02/MMseq2/results/taxo_results.tsv"
+
     if args.out:
         out = args.out
     else:
         out="module_02/MMseq2/results/"
+
     if not os.path.exists(out):
         os.makedirs(out)
+
+
+    data = building_TSV(path)
+
+    data.to_csv(f"{out}/taxo_seeds.tsv", sep="\t", index=False)
+
+    print(f"Job finish ! File created : {out}/taxo_seeds.tsv")
