bd3b386371952a72965e7798bbcaae83ff98643f
HugoS63 <naarutsu@outlook.fr>
2025-02-18 14:17:41 +0100

    add multi ecosystems analysis

--- module_02/MMseq2/bin/building_res.py
+++ module_02/MMseq2/bin/building_res.py
@@ -3,6 +3,7 @@
 import argparse
 import pandas as pd
 import os
+import glob
 
 
 def building_TSV(path):
@@ -13,12 +14,12 @@ def building_TSV(path):
         for line in f:
             line = line.rstrip().split("\t")
             taxo_dict = {"ID": line[0], "Domain": "NA", "Kingdom": "NA", "Phylum": "NA", "Class": "NA",
-                     "Order": "NA", "Family": "NA", "Genus": "NA", "Species": "NA"}
+                         "Order": "NA", "Family": "NA", "Genus": "NA", "Species": "NA"}
 
             for rank in line[-1].split(";"):
                 if rank[0] in ["d", "k", "p", "c", "o", "f", "g", "s"]:
                     key = {"d": "Domain", "k": "Kingdom", "p": "Phylum", "c": "Class", "o": "Order",
-                       "f": "Family", "g": "Genus", "s": "Species"}[rank[0]]
+                           "f": "Family", "g": "Genus", "s": "Species"}[rank[0]]
                     taxo_dict[key] = rank[2:]
             data.append(taxo_dict)
 
@@ -30,29 +31,32 @@ def building_TSV(path):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
-        description = """This script aims to create some figures using the quality reports from CheckV analysis.""")
+        description="This script processes multiple TSV files from CheckV analysis results.")
 
-    parser.add_argument("--path", "-p", help="Defines the path where the TSV taxo results is located. Usage : -p path folder/", type=str)
-    parser.add_argument("--out", "-o", help="Defines the path where the rebuilding TSV created can be stored. Usage : -o path folder/", type=str)
+    parser.add_argument("--input_dir", "-i", help="Path to the folder containing TSV files. Default: module_02/MMseq2/results/biome_taxo/", type=str, default="module_02/MMseq2/results/biome_taxo/")
+    parser.add_argument("--output_dir", "-o", help="Path to the output folder. Default: module_02/MMseq2/results/", type=str, default="module_02/MMseq2/results/rebuild_taxo")
 
     args = parser.parse_args()
 
-    if args.path:
-        path = args.path
+    input_dir = args.input_dir
+    output_dir = args.output_dir
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    tsv_files = glob.glob(os.path.join(input_dir, "*.tsv"))
+
+    if not tsv_files:
+        print(f"No file .tsv found here :  {input_dir}")
     else:
-        path="module_02/MMseq2/results/taxo_results.tsv"
+        for file_path in tsv_files:
+            file_name = os.path.basename(file_path)
+            prefix = file_name.split("_")[0]
+            output_file = os.path.join(output_dir, f"{prefix}_taxo_seeds.tsv")
 
-    if args.out:
-        out = args.out
-    else:
-        out="module_02/MMseq2/results/"
+            print(f"Processing {file_path} → {output_file}")
 
-    if not os.path.exists(out):
-        os.makedirs(out)
+            data = building_TSV(file_path)
+            data.to_csv(output_file, sep="\t", index=False)
 
-
-    data = building_TSV(path)
-
-    data.to_csv(f"{out}/taxo_seeds.tsv", sep="\t", index=False)
-
-    print(f"Job finish ! File created : {out}/taxo_seeds.tsv")
+        print("Job done  !")
