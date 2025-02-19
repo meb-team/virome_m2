d38ddfcfcccd5ba6337e33fb95254ed0845e3548
HugoS63 <naarutsu@outlook.fr>
2025-02-19 09:22:24 +0100

    multi ecosystemic analysis

--- module_02/MMseq2/bin/sankey.R
+++ module_02/MMseq2/bin/sankey.R
@@ -1,82 +1,43 @@
-#script_sankey.R
-
-#command shell : R --vanilla < script_sankey <_input_report_>
-
-if (!require(remotes)) { install.packages("remotes") }
-remotes::install_github("fbreitwieser/pavian")
-
-packages <- c("htmlwidgets", "webshot")
-
-install_if_missing <- function(pkg) {
-  if (!requireNamespace(pkg, quietly = TRUE)) {
-    install.packages(pkg, dependencies = TRUE)
-  }
-  library(pkg, character.only = TRUE)
-}
-
-sapply(packages, install_if_missing)
-
-webshot::install_phantomjs()
+# Charger les bibliothèques
 library("htmlwidgets")
 library("pavian")
 library("webshot")
 
-
-# https://github.com/fbreitwieser/pavian/blob/master/R/sample-build_sankey_network.R
-
-
-build_sankey_network <- function(my_report, taxRanks =  c("D","K","P","C","O","F","G","S"), maxn=10,
-                                 zoom = F, title = NULL,
-                                 ...) {
+# Fonction pour générer le diagramme Sankey
+build_sankey_network <- function(my_report, taxRanks = c("D","K","P","C","O","F","G","S"), maxn=10,
+                                 zoom = FALSE, title = NULL, ...) {
   stopifnot("taxRank" %in% colnames(my_report))
   if (!any(taxRanks %in% my_report$taxRank)) {
-    warning("report does not contain any of the taxRanks - skipping it")
+    warning("Le rapport ne contient aucun des taxRanks spécifiés - Ignoré")
     return()
   }
+  
   my_report <- subset(my_report, taxRank %in% taxRanks)
   my_report <- plyr::ddply(my_report, "taxRank", function(x) x[utils::tail(order(x$cladeReads,-x$depth), n=maxn), , drop = FALSE])
-  
   my_report <- my_report[, c("name","taxLineage","taxonReads", "cladeReads","depth", "taxRank")]
-  
   my_report <- my_report[!my_report$name %in% c('-_root'), ]
-  #my_report$name <- sub("^-_root.", "", my_report$name)
   
   splits <- strsplit(my_report$taxLineage, "\\|")
-  
-  ## for the root nodes, we'll have to add an 'other' link to account for all cladeReads
   root_nodes <- sapply(splits[sapply(splits, length) ==2], function(x) x[2])
-  
   sel <- sapply(splits, length) >= 3
   splits <- splits[sel]
   
-  links <- data.frame(do.call(rbind,
-                              lapply(splits, function(x) utils::tail(x[x %in% my_report$name], n=2))), stringsAsFactors = FALSE)
+  links <- data.frame(do.call(rbind, lapply(splits, function(x) utils::tail(x[x %in% my_report$name], n=2))), stringsAsFactors = FALSE)
   colnames(links) <- c("source","target")
   links$value <- my_report[sel,"cladeReads"]
   
   my_taxRanks <- taxRanks[taxRanks %in% my_report$taxRank]
   taxRank_to_depth <- stats::setNames(seq_along(my_taxRanks)-1, my_taxRanks)
-    
+  
   nodes <- data.frame(name=my_report$name,
                       depth=taxRank_to_depth[my_report$taxRank],
                       value=my_report$cladeReads,
                       stringsAsFactors=FALSE)
   
-  for (node_name in root_nodes) {
-    diff_sum_vs_all <- my_report[my_report$name == node_name, "cladeReads"] - sum(links$value[links$source == node_name])
-    if (diff_sum_vs_all > 0) {
-      nname <- paste("other", sub("^._","",node_name))
-      #nname <- node_name
-      #links <- rbind(links, data.frame(source=node_name, target=nname, value=diff_sum_vs_all, stringsAsFactors = FALSE))
-      #nodes <- rbind(nodes, nname)
-    }
-  }
-  
   names_id = stats::setNames(seq_len(nrow(nodes)) - 1, nodes[,1])
   links$source <- names_id[links$source]
   links$target <- names_id[links$target]
   links <- links[links$source != links$target, ]
-  
   nodes$name <- sub("^._","", nodes$name)
   links$source_name <- nodes$name[links$source + 1]
   
@@ -112,21 +73,30 @@ build_sankey_network <- function(my_report, taxRanks =  c("D","K","P","C","O","F
     )
 }
 
-file <- commandArgs()[3]
+# Récupérer le fichier passé en argument
+file <- commandArgs(trailingOnly = TRUE)[1]
 
+# Définir le dossier de sortie
+output_dir <- file.path(dirname(file), "../sankey")
+if (!dir.exists(output_dir)) {
+  dir.create(output_dir, recursive = TRUE)
+}
+
+# Extraire le nom du fichier sans le chemin
+file_basename <- basename(file)
+
+# Définir les chemins de sortie
+output1 <- file.path(output_dir, paste0("sankey_", file_basename, ".html"))
+output2 <- file.path(output_dir, paste0("sankey_", file_basename, ".pdf"))
+
+print(paste("Fichier HTML de sortie :", output1))
+print(paste("Fichier PDF de sortie :", output2))
+
+# Lire le rapport et générer le Sankey
 sank <- build_sankey_network(read_report(file))
 
-output1=paste("sankey_",file,".html",sep="")
-output2=paste("sankey_",file,".pdf",sep="")
+# Sauvegarde du fichier HTML
+saveWidget(sank, output1, selfcontained = TRUE, libdir = NULL, background = "white", title = class(sank)[[1]], knitrOptions = list())
 
-saveWidget(                                        #save an html file of sankey
-  sank,
-  output1,
-  selfcontained = FALSE,
-  libdir = NULL,
-  background = "white",
-  title = class(sank)[[1]],
-  knitrOptions = list()  
-)
-
-webshot(output1,output2)                          #save in pdf format
+# Sauvegarde en PDF
+webshot(output1, output2)
