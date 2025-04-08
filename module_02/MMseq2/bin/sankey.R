203ac4eea29e9298cb9cb68e2fe1f6038a428f5b
HugoBioInf <naarutsu@outlook.fr>
2025-04-08 08:54:09 +0200

    fix

--- module_02/MMseq2/bin/sankey.R
+++ module_02/MMseq2/bin/sankey.R
@@ -21,7 +21,7 @@ build_sankey_network <- function(my_report, taxRanks = c("D","K","P","C","O","F"
   }
   
   my_report <- subset(my_report, taxRank %in% taxRanks)
-  my_report <- plyr::ddply(my_report, "taxRank", function(x) x[utils::tail(order(x$cladeReads,-x$depth), n=maxn), , drop = FALSE])
+  my_report <- plyr::ddply(my_report, "taxRank", function(x) x[utils::tail(order(x$cladeReads,-x$depth), n=20), , drop = FALSE])
   my_report <- my_report[, c("name","taxLineage","taxonReads", "cladeReads","depth", "taxRank")]
   my_report <- my_report[!my_report$name %in% c('-_root'), ]
   
